0ba6f6b9a27bbfd72062bfe0c325342296bb2020
Luis G. Martinez <99708664+luis-guillermo-martinez@users.noreply.github.com>
2022-08-12 07:38:37 -0300

    Update AuthController.java

--- src/main/java/com/lgm/LGM_Portfolio/Security/Controller/AuthController.java
+++ src/main/java/com/lgm/LGM_Portfolio/Security/Controller/AuthController.java
@@ -30,7 +30,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "https://backendlgm.herokuapp.com")
+@CrossOrigin
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
