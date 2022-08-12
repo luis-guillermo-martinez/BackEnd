f7a0d635d80c58c95ddf04da92fd5274129de16d
Luis G. Martinez <99708664+luis-guillermo-martinez@users.noreply.github.com>
2022-08-12 01:39:23 -0300

    Update CEducacion.java

--- src/main/java/com/lgm/LGM_Portfolio/Controller/CEducacion.java
+++ src/main/java/com/lgm/LGM_Portfolio/Controller/CEducacion.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/educacion")
-@CrossOrigin(origins = "https://backportflgm.herokuapp.com")
+@CrossOrigin(origins = "https://backendlgm.herokuapp.com")
 public class CEducacion {
     @Autowired
     Seducacion sEducacion;
