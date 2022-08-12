a986125646dca8d86c9e42d7af3a00b36c69dae1
Luis G. Martinez <99708664+luis-guillermo-martinez@users.noreply.github.com>
2022-08-12 02:07:42 -0300

    make

--- src/main/java/com/lgm/LGM_Portfolio/Controller/PersonaController.java
+++ src/main/java/com/lgm/LGM_Portfolio/Controller/PersonaController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "https://backendlgm.herokuapp.com")
+@CrossOrigin(origins = "https://portfolio-lgm-2469a.web.app")
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
